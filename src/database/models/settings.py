2137c262f9e4fc20dd8b5fb2dfe9504de0e36e07
Ivan <ikrakovskij@aouniqo.ru>
2024-11-04 19:29:50 +0800

    fix(Settings model)

--- src/database/models/settings.py
+++ src/database/models/settings.py
@@ -1,8 +1,8 @@
 from sqlalchemy import update
 from sqlalchemy.orm import Mapped, mapped_column
 
-from src.schemas import SettingsSchema
 from src.database.connection import Base, async_session_maker
+from src.schemas import SettingsSchema
 
 
 class Settings(Base):
@@ -25,18 +25,33 @@ class Settings(Base):
     @classmethod
     async def set_settings(
             cls, dynamic_button_count: int = None,
-            dynamic_welcome_message: str = None
+            welcome_message: str = None
     ):
+        """
+       Updates the dynamic button count or welcome message.
+
+       Parameters:
+       ----------
+       dynamic_button_count : int, optional
+           Number of dynamic buttons to display.
+       welcome_message : str, optional
+           Text for the welcome message.
+
+       Notes:
+       -----
+       Only updates the record if at least one parameter is provided.
+       """
         async with async_session_maker() as session:
             if await session.get(cls, 1):
-                if not (dynamic_button_count or dynamic_welcome_message):
+                if not (dynamic_button_count or welcome_message):
                     return
                 query = update(cls).where(cls.id == 1)
                 if dynamic_button_count:
                     query = query.values(dynamic_button_count=dynamic_button_count)
-                if dynamic_welcome_message:
+                if welcome_message:
                     query = query.values(
-                        dynamic_welcome_message=dynamic_welcome_message)
+                        welcome_message=welcome_message)
                 await session.execute(
                     query
                 )
+                await session.commit()
