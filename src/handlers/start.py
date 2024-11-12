fdc43e9a3f7bac48044204318d14b61de662401e
Yaroslav <lysov.yaroslav.r@mail.ru>
2024-11-12 22:10:53 +0800

    refactor(start): simplify start command logic
    - Removed admin-specific logic from `/start` command handler.
    - Simplified user greeting and database addition logic for new users.
    - Updated greeting messages for new and existing users.
    - Refactored database logging to match updated message text.

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,35 +1,30 @@
 from aiogram import F, Router
 from aiogram.types import Message
 from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
+
 
 from src.config import logger
-from src.config import BOT_ADMIN_ID
 from src.database.models import DbUser
-from src.utils.keyboards.admin import admin_panel_kb
+
 
 router = Router()
 
-
 @router.message(Command('start'), F.chat.type == 'private')
-async def start(message: Message, state: FSMContext):
+async def start(message: Message):
     if not await DbUser.get_user(user_id=message.from_user.id):
         await DbUser.add_user(
             user_id=message.from_user.id,
             full_name=message.from_user.first_name
         )
         logger.debug(
-            f'Пользователь({message.from_user.full_name}) c id: {message.from_user.id} добавлен в базу данных')
+            f'Пользователь({message.from_user.full_name}) с id: {message.from_user.id} добавлен в БД')
 
         await message.answer(
-            'Привет, ты новенький, видимо, этот бот доступен только админам '
-            'Когда-нибудь потом мы будем через него делать рассылки'
+            'Салам, пока что этот бот доступен только для администраторов, позже мы будем '
+            'делать рассылки через него'
         )
     else:
-        if message.chat.type == 'private' and message.from_user.id == BOT_ADMIN_ID:
-            await message.answer(
-                text = 'Привет! '
-                       'Тут ты можешь настроить приветственное сообщение',
-                reply_markup = admin_panel_kb
-            )
+        await message.answer(
+            'Когда добавим - будет рассылка)) Жди)'
+        )
     await message.delete()
