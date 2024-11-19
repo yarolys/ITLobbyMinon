4bc74a6bf14a1e47246d1859b4e6593b85da835d
Yaroslav <lysov.yaroslav.r@mail.ru>
2024-11-19 16:31:36 +0800

    fix: reorder router inclusion in main.py
    - Adjusted the order of router inclusion in `main.py` to maintain consistency and readability.
    - Specifically, moved `welcome_message_router` to align with other admin panel routers.
    
    This change ensures logical grouping of related routers for better maintainability.

--- run.py
+++ run.py
@@ -8,15 +8,16 @@ from src.handlers.join2group import router as join2group_router
 from src.handlers.start import router as start_router
 from src.config import logger
 
+
 async def main():
     dp = Dispatcher()
     dp.include_routers(
         join2group_router,
         start_router,
         admin_panel.admin_panel_router,
-        admin_panel.welcome_message_router,
         admin_panel.static_buttons_router,
-        admin_panel.dynamic_buttons_router
+        admin_panel.dynamic_buttons_router,
+        admin_panel.welcome_message_router
     )
 
     r = await bot.get_me()
